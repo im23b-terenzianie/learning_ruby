58e905cc4697cf8202b3f5f3fb0982581dc23f58
im23b-terenzianie <terenzianie@bzz.ch>
2025-06-19 13:53:23 +0200

    Hello World & small guessing game created

---
+++ Basics/Guessgame.rb
@@ -0,0 +1,15 @@
+randomnum = rand(1..100)
+userguess = 0
+puts "Welcome to the guessing Game"
+while userguess != randomnum do 
+    puts "Guess a number between 1 and 100"
+    userguess = gets.to_i
+    if userguess < randomnum then
+        puts "your number is smaller"
+    elsif userguess > randomnum then 
+        puts "your number is bigger"
+    else 
+        puts "Correct!!"
+    end
+end 
+
